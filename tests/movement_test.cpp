e4a30fe5db56475e9c8ca0a60c696b89f9834ed2
Loszi <jiozzi11@gmail.com>
2024-05-30 22:17:40 -0400

    finished piece movement tests, still need to add unit tests for rest of movement file

--- tests/movement_test.cpp
+++ tests/movement_test.cpp
@@ -35,36 +35,8 @@ TEST(Movement, MovePiece) {
         {0, 1, 1, 1, 1, 1, 1, 1},
         {4, 3, 2, 5, 6, 2, 3, 4},
     };
-
-    move_piece(60, 40, board); //pawn
+    move_piece(60, 40, board); //moving single piece
     SAME_BOARD(board, result1);
-
-    move_piece(10, 20, board); //pawn
-    int result2[8][8] = {
-        {-4, -3, -2, -5, -6, -2, -3, -4},
-        {0, -1, -1, -1, -1, -1, -1, -1},
-        {-1, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0},
-        {1, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 1, 1, 1, 1, 1, 1, 1},
-        {4, 3, 2, 5, 6, 2, 3, 4},
-    };
-    SAME_BOARD(board, result2);
-
-    move_piece(70, 50, board); //rook
-    int result3[8][8] = {
-        {-4, -3, -2, -5, -6, -2, -3, -4},
-        {0, -1, -1, -1, -1, -1, -1, -1},
-        {-1, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0},
-        {1, 0, 0, 0, 0, 0, 0, 0},
-        {4, 0, 0, 0, 0, 0, 0, 0},
-        {0, 1, 1, 1, 1, 1, 1, 1},
-        {0, 3, 2, 5, 6, 2, 3, 4},
-    };
-    SAME_BOARD(board, result3);
-
     //castling
     int board2[8][8] = {
         {-4, 0, 0, 0, -6, -2, -3, -4},
@@ -88,7 +60,6 @@ TEST(Movement, MovePiece) {
         {4, 3, 2, 5, 6, 0, 0, 4},
     };
     SAME_BOARD(board2, result4);
-
     move_piece(74, 76, board2); //white right castle
     int result5[8][8] = {
         {0, 0, -6, -4, 0, -2, -3, -4},
@@ -101,7 +72,40 @@ TEST(Movement, MovePiece) {
         {4, 3, 2, 5, 0, 4, 6, 0},
     };
     SAME_BOARD(board2, result5);
-
+    int board10[8][8] = {
+        {-4, 0, 0, 0, -6, 0, 0, -4},
+        {-1, -1, -1, -1, -1, -1, -1, -1},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {1, 1, 1, 1, 1, 1, 1, 1},
+        {4, 0, 0, 0, 6, 0, 0, 4},
+    };
+    move_piece(4, 6, board10); //black  right castle
+    int result10[8][8] = {
+        {-4, 0, 0, 0, 0, -4, -6, 0},
+        {-1, -1, -1, -1, -1, -1, -1, -1},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {1, 1, 1, 1, 1, 1, 1, 1},
+        {4, 0, 0, 0, 6, 0, 0, 4},
+    };
+    SAME_BOARD(board10, result10);
+    move_piece(74, 72, board10); //white left castle
+    int result11[8][8] = {
+        {-4, 0, 0, 0, 0, -4, -6, 0},
+        {-1, -1, -1, -1, -1, -1, -1, -1},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {1, 1, 1, 1, 1, 1, 1, 1},
+        {0, 0, 6, 4, 0, 0, 0, 4},
+    };
+    SAME_BOARD(board10, result11);
     //en passant
     int board3[8][8] = {
         {-4, -3, -2, -5, -6, -2, -3, -4},
@@ -125,7 +129,6 @@ TEST(Movement, MovePiece) {
         {4, 3, 2, 5, 6, 2, 3, 4},
     };
     SAME_BOARD(board3, result6);
-
     int board4[8][8] = {
         {-4, 0, -2, -5, -6, -2, -3, -4},
         {-1, -1, -1, 0, -1, -1, -1, -1},
@@ -148,8 +151,54 @@ TEST(Movement, MovePiece) {
         {4, 3, 2, 5, 6, 2, 3, 4},
     };
     SAME_BOARD(board4, result7);
+    //en passant black
+    int board20[8][8] = {
+        {-4, -3, -2, -5, -6, -2, -3, -4},
+        {-1, -1, -1, 0, -1, -1, -1, -1},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 1, -1, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {1, 1, 1, 0, 1, 1, 1, 1},
+        {4, 3, 2, 5, 6, 2, 3, 4},
+    };
+    move_piece(44, 53, board20);
+    int result20[8][8] = {
+        {-4, -3, -2, -5, -6, -2, -3, -4},
+        {-1, -1, -1, 0, -1, -1, -1, -1},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, -1, 0, 0, 0, 0},
+        {1, 1, 1, 0, 1, 1, 1, 1},
+        {4, 3, 2, 5, 6, 2, 3, 4},
+    };
+    SAME_BOARD(board20, result20);
+    int board21[8][8] = {
+        {-4, -3, -2, -5, -6, -2, -3, -4},
+        {-1, -1, -1, 0, -1, -1, -1, -1},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 1, -1, 0, 0, 0},
+        {0, 0, 0, 2, 0, 0, 0, 0},
+        {1, 1, 1, 0, 1, 1, 1, 1},
+        {4, 3, 0, 5, 6, 2, 3, 4},
+    };
+    move_piece(44, 53, board21);
+    int result21[8][8] = {
+        {-4, -3, -2, -5, -6, -2, -3, -4},
+        {-1, -1, -1, 0, -1, -1, -1, -1},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 1, 0, 0, 0, 0},
+        {0, 0, 0, -1, 0, 0, 0, 0},
+        {1, 1, 1, 0, 1, 1, 1, 1},
+        {4, 3, 0, 5, 6, 2, 3, 4},
+    };
+    SAME_BOARD(board21, result21);
 }
 
+//for movement test going off board, capturing enemy, and being blocked by ally (white and black)
 TEST(Movement, Pawn) {
     int board[8][8] = {
         {-4, -3, -2, -5, -6, -2, -3, -4},
@@ -182,11 +231,12 @@ TEST(Movement, Pawn) {
     ASSERT_EQ(moves, correct);
     correct.clear();
     moves.clear();
+
     //only one square now
     int board2[8][8] = {
         {-4, -3, -2, -5, -6, -2, -3, -4},
-        {-1, -1, -1, -1, -1, -1, -1, -1},
-        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, -1, -1, -1, -1, -1, -1, -1},
+        {-1, 0, 0, 0, 0, 0, 0, 0},
         {0, 0, 0, 0, 0, 0, 0, 0},
         {0, 0, 0, 0, 0, 0, 0, 0},
         {1, 0, 0, 0, 0, 0, 0, 0},
@@ -200,35 +250,78 @@ TEST(Movement, Pawn) {
     ASSERT_EQ(moves, correct);
     correct.clear();
     moves.clear();
+    w_turn = false; //black
+    i = 2;
+    get_pawn_moves(board2, i, j, w_turn, moves, en_passant);
+    correct.push_back(30);
+    ASSERT_EQ(moves, correct);
+    correct.clear();
+    moves.clear();
 
-    //capute
-    int board3[8][8] = {
-        {-4, 0, -2, -5, -6, -2, -3, -4},
+    //ally piece
+    int board10[8][8] = {
+        {-4, -3, -2, -5, -6, -2, -3, -4},
         {-1, -1, -1, -1, -1, -1, -1, -1},
         {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 2, 0, -1, 0, 0},
+        {0, 0, 0, 1, 0, -2, 0, 0},
         {0, 0, 0, 0, 0, 0, 0, 0},
+        {1, 1, 1, 1, 1, 1, 1, 1},
+        {4, 3, 2, 5, 6, 2, 3, 4},
+    };
+    w_turn = true;
+    i = 4;
+    j = 3;
+    get_pawn_moves(board10, i, j, w_turn, moves, en_passant);
+    ASSERT_TRUE(moves.empty());
+    w_turn = false;
+    i = 3;
+    j = 5;
+    get_pawn_moves(board10, i, j, w_turn, moves, en_passant);
+    ASSERT_TRUE(moves.empty());
+
+    //capture
+    int board3[8][8] = {
+        {-4, 0, -2, -5, -6, -2, -3, -4},
+        {-1, -1, -1, -1, -1, -1, -1, 0},
+        {0, 0, 0, 0, 0, 0, 0, -1},
+        {0, 0, 0, 0, 0, 0, 3, 0},
         {0, -3, 0, 0, 0, 0, 0, 0},
         {1, 0, 0, 0, 0, 0, 0, 0},
         {0, 1, 1, 1, 1, 1, 1, 1},
-        {4, 3, 2, 5, 6, 2, 3, 4},
+        {4, 3, 2, 5, 6, 2, 0, 4},
     };
+    w_turn = true;
+    i = 5;
+    j = 0;
     get_pawn_moves(board3, i, j, w_turn, moves, en_passant);
     correct.push_back(40);
     correct.push_back(41);
     ASSERT_EQ(moves, correct);
     correct.clear();
     moves.clear();
+    w_turn = false;
+    i = 2;
+    j = 7;
+    get_pawn_moves(board3, i, j, w_turn, moves, en_passant);
+    correct.push_back(37);
+    correct.push_back(36);
+    ASSERT_EQ(moves, correct);
+    correct.clear();
+    moves.clear();
+
     //en passante
     int board4[8][8] = {
         {-4, -3, -2, -5, -6, -2, -3, -4},
-        {-1, -1, -1, 0, -1, -1, -1, -1},
+        {-1, 0, -1, 0, -1, -1, -1, -1},
         {0, 0, 0, 0, 0, 0, 0, 0},
         {0, 0, 0, 1, -1, 0, 0, 0},
+        {0, -1, 1, 0, 0, 0, 0, 0},
         {0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0},
-        {1, 1, 1, 0, 1, 1, 1, 1},
+        {1, 1, 0, 0, 1, 1, 1, 1},
         {4, 3, 2, 5, 6, 2, 3, 4},
     };
+    w_turn = true;
     en_passant = 24;
     i = 3;
     j = 3;
@@ -236,10 +329,20 @@ TEST(Movement, Pawn) {
     correct.push_back(23);
     correct.push_back(24);
     ASSERT_EQ(moves, correct);
+    moves.clear();
+    correct.clear();
+
+    w_turn = false;
+    en_passant = 52;
+    i = 4;
+    j = 1;
+    get_pawn_moves(board4, i, j, w_turn, moves, en_passant);
+    correct.push_back(51);
+    correct.push_back(52);
+    ASSERT_EQ(moves, correct);
 }
 
 TEST(Movement, Bishop) {
-    //test with and bishop, add enemy stopping line of sight and ally blocking completely
     int board[8][8] = {
         {-4, -3, -2, -5, -6, -2, -3, -4},
         {-1, -1, -1, -1, -1, -1, -1, -1},
@@ -258,6 +361,11 @@ TEST(Movement, Bishop) {
     get_bishop_moves(board, i, j, w_turn, moves);
     ASSERT_TRUE(moves.empty()); 
 
+    w_turn = false;
+    i = 0;
+    get_bishop_moves(board, i, j, w_turn, moves);
+    ASSERT_TRUE(moves.empty()); 
+
     int board1[8][8] = {
         {-4, -3, 0, -5, -6, -2, -3, -4},
         {-1, -1, -1, -1, -1, -1, -1, -1},
@@ -268,6 +376,7 @@ TEST(Movement, Bishop) {
         {1, 1, 1, 1, 1, 1, 1, 1},
         {4, 3, 0, 5, 6, 2, 3, 4},
     };
+    w_turn = true;
     i = 4;
     j = 3; //white bishop
     correct.push_back(32);
@@ -281,16 +390,6 @@ TEST(Movement, Bishop) {
     correct.clear();
     moves.clear();
 
-    int board2[8][8] = {
-        {-4, -3, 0, -5, -6, -2, -3, -4},
-        {-1, -1, -1, -1, -1, -1, -1, -1},
-        {0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, -2, 0, 0, 0},
-        {0, 0, 0, 2, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0},
-        {1, 1, 1, 1, 1, 1, 1, 1},
-        {4, 3, 0, 5, 6, 2, 3, 4},
-    };
     i = 3;
     j = 4; //black bishop
     w_turn = false;
@@ -300,10 +399,318 @@ TEST(Movement, Bishop) {
     correct.push_back(45);
     correct.push_back(56);
     correct.push_back(67);
-    get_bishop_moves(board2, i, j, w_turn, moves);
+    get_bishop_moves(board1, i, j, w_turn, moves);
     ASSERT_EQ(moves, correct);
 }
 
-/*Test(Movement, Knight) {
+TEST(Movement, Knight) {
+    int board[8][8] = {
+        {-4, -3, -2, -5, -6, -2, -3, -4},
+        {-1, -1, -1, -1, -1, -1, -1, -1},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {1, 1, 1, 1, 1, 1, 1, 1},
+        {4, 3, 2, 5, 6, 2, 3, 4},
+    };
+    std::vector<int> moves;
+    std::vector<int> correct;
+    bool w_turn = true;
+    int i = 7;
+    int j = 1;
+    get_knight_moves(board, i, j, w_turn, moves);
+    correct.push_back(50);
+    correct.push_back(52);
+    ASSERT_EQ(moves, correct); 
+    correct.clear();
+    moves.clear();
 
-}*/
+    w_turn = false;
+    i = 0;
+    get_knight_moves(board, i, j, w_turn, moves);
+    correct.push_back(20);
+    correct.push_back(22);
+    ASSERT_EQ(moves, correct); 
+    correct.clear();
+    moves.clear();
+
+
+    int board2[8][8] = {
+        {-4, -3, -2, -5, -6, -2, -3, -4},
+        {-1, -1, -1, -1, -1, -1, -1, -1},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 3, 0, 0, 0, 0},
+        {0, 0, 0, 0, -3, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {1, 1, 1, 1, 1, 1, 1, 1},
+        {4, 0, 2, 5, 6, 2, 3, 4},
+    };
+    w_turn = true;
+    i = 3;
+    j = 3;
+    get_knight_moves(board, i, j, w_turn, moves);
+    correct.push_back(12);
+    correct.push_back(14);
+    correct.push_back(25);
+    correct.push_back(45);
+    correct.push_back(52);
+    correct.push_back(54);
+    correct.push_back(21);
+    correct.push_back(41);
+    ASSERT_EQ(moves, correct); 
+    correct.clear();
+    moves.clear();
+    
+    w_turn = false;
+    i = 4;
+    j = 4;
+    get_knight_moves(board, i, j, w_turn, moves);
+    correct.push_back(23);
+    correct.push_back(25);
+    correct.push_back(36);
+    correct.push_back(56);
+    correct.push_back(63);
+    correct.push_back(65);
+    correct.push_back(32);
+    correct.push_back(52);
+    ASSERT_EQ(moves, correct); 
+}
+
+TEST(Movement, Rook) {
+    int board[8][8] = {
+        {-4, -3, -2, -5, -6, -2, -3, -4},
+        {-1, -1, -1, -1, -1, -1, -1, -1},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {1, 1, 1, 1, 1, 1, 1, 1},
+        {4, 3, 2, 5, 6, 2, 3, 4},
+    };
+    std::vector<int> moves;
+    std::vector<int> correct;
+    bool w_turn = true;
+    int i = 7;
+    int j = 0;
+    get_rook_moves(board, i, j, w_turn, moves);
+    ASSERT_TRUE(moves.empty()); 
+
+    w_turn = false;
+    i = 0;
+    get_rook_moves(board, i, j, w_turn, moves);
+    ASSERT_TRUE(moves.empty()); 
+
+    int board2[8][8] = {
+        {0, -3, -2, -5, -6, -2, -3, -4},
+        {-1, -1, -1, -1, -1, -1, -1, -1},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, -4, 0, 0, 0},
+        {0, 0, 0, 4, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {1, 1, 1, 1, 1, 1, 1, 1},
+        {0, 3, 2, 5, 6, 2, 3, 4},
+    };
+
+    w_turn = true;
+    i = 4;
+    j = 3;
+    get_rook_moves(board, i, j, w_turn, moves);
+    correct.push_back(33);
+    correct.push_back(23);
+    correct.push_back(13);
+    correct.push_back(44);
+    correct.push_back(45);
+    correct.push_back(46);
+    correct.push_back(47);
+    correct.push_back(53);
+    correct.push_back(42);
+    correct.push_back(41);
+    correct.push_back(40);
+    ASSERT_EQ(moves, correct);
+    moves.clear();
+    correct.clear();
+
+    w_turn = false;
+    i = 3;
+    j = 4;
+    get_rook_moves(board, i, j, w_turn, moves);
+    correct.push_back(24);
+    correct.push_back(35);
+    correct.push_back(36);
+    correct.push_back(37);
+    correct.push_back(44);
+    correct.push_back(54);
+    correct.push_back(64);
+    correct.push_back(33);
+    correct.push_back(32);
+    correct.push_back(31);
+    correct.push_back(30);
+    ASSERT_EQ(moves, correct);
+}
+
+TEST(Movement, King) {
+    int board[8][8] = {
+        {-4, -3, -2, -5, -6, -2, -3, -4},
+        {-1, -1, -1, -1, -1, -1, -1, -1},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {1, 1, 1, 1, 1, 1, 1, 1},
+        {4, 3, 2, 5, 6, 2, 3, 4},
+    };
+    std::vector<int> moves;
+    std::vector<int> correct;
+    bool w_turn = true;
+    int i = 7;
+    int j = 4;
+    bool castle[4] = {false};
+    get_king_moves(board, i, j, w_turn, moves, castle);
+    ASSERT_TRUE(moves.empty()); 
+
+    w_turn = false;
+    i = 0;
+    get_king_moves(board, i, j, w_turn, moves, castle);
+    ASSERT_TRUE(moves.empty()); 
+
+    //capture
+    int board2[8][8] = {
+        {-4, -3, -2, -5, -6, -2, -3, -4},
+        {-1, -1, -1, -1, -1, -1, -1, -1},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, -6, 0, 0, 0},
+        {0, 0, 0, 6, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {1, 1, 1, 1, 1, 1, 1, 1},
+        {4, 3, 2, 5, 6, 2, 3, 4},
+    };
+    w_turn = true;
+    i = 4;
+    j = 3;
+    get_king_moves(board2, i, j, w_turn, moves, castle);
+    correct.push_back(32);
+    correct.push_back(33);
+    correct.push_back(34);
+    correct.push_back(52);
+    correct.push_back(53);
+    correct.push_back(54);
+    correct.push_back(44);
+    correct.push_back(42);
+    ASSERT_EQ(moves, correct);
+    moves.clear();
+    correct.clear();
+
+    w_turn = false;
+    i = 3;
+    j = 4;
+    get_king_moves(board2, i, j, w_turn, moves, castle);
+    correct.push_back(23);
+    correct.push_back(24);
+    correct.push_back(25);
+    correct.push_back(43);
+    correct.push_back(44);
+    correct.push_back(45);
+    correct.push_back(35);
+    correct.push_back(33);
+    ASSERT_EQ(moves, correct);
+    moves.clear();
+    correct.clear();
+
+    //castle right
+    int board3[8][8] = {
+        {-4, 0, 0, 0, -6, 0, 0, -4},
+        {-1, -1, -1, -1, -1, -1, -1, -1},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, -6, 0, 0, 0},
+        {0, 0, 0, 6, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {1, 1, 1, 1, 1, 1, 1, 1},
+        {4, 0, 0, 0, 6, 0, 0, 4},
+    };
+    castle[0] = true;
+    w_turn = true;
+    i = 7;
+    j = 4;
+    get_king_moves(board3, i, j, w_turn, moves, castle);
+    correct.push_back(75);
+    correct.push_back(73);
+    ASSERT_EQ(moves, correct);
+    moves.clear();
+    castle[1] = true;
+    get_king_moves(board3, i, j, w_turn, moves, castle);
+    correct.push_back(76);
+    ASSERT_EQ(moves, correct);
+    moves.clear();
+    correct.clear();
+
+    //black
+    castle[1] = false;
+    w_turn = false;
+    i = 0;
+    get_king_moves(board3, i, j, w_turn, moves, castle);
+    correct.push_back(5);
+    correct.push_back(3);
+    ASSERT_EQ(moves, correct);
+    moves.clear();
+    castle[1] = true;
+    get_king_moves(board3, i, j, w_turn, moves, castle);
+    correct.push_back(6);
+    ASSERT_EQ(moves, correct);
+    moves.clear();
+    correct.clear();
+    
+    //rest
+    castle[0] = false;
+    castle[1] = false;
+    //only set one right and make sure doesnt work
+    castle[2] = true;
+    w_turn = true;
+    i = 7;
+    get_king_moves(board3, i, j, w_turn, moves, castle);
+    correct.push_back(75);
+    correct.push_back(73);
+    ASSERT_EQ(moves, correct);
+    moves.clear();
+    castle[3] = true;
+    get_king_moves(board3, i, j, w_turn, moves, castle);
+    correct.push_back(72);
+    ASSERT_EQ(moves, correct);
+    moves.clear();
+    correct.clear();
+    castle[3] = false;
+
+    //black left
+    w_turn = false;
+    i = 0;
+    get_king_moves(board3, i, j, w_turn, moves, castle);
+    correct.push_back(5);
+    correct.push_back(3);
+    ASSERT_EQ(moves, correct);
+    moves.clear();
+    castle[3] = true;
+    get_king_moves(board3, i, j, w_turn, moves, castle);
+    correct.push_back(2);
+    ASSERT_EQ(moves, correct);
+    moves.clear();
+    correct.clear();
+}
+
+TEST(Movement, Trajectory) {
+    //make sure get_trajectory == get_piece_moves at the right pos
+    int board[8][8] = {
+        {-4, -3, -2, -5, -6, -2, -3, -4},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {-1, -1, -1, -1, -1, -1, -1, -1},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {1, 1, 1, 1, 1, 1, 1, 1},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {4, 3, 2, 5, 6, 2, 3, 4},
+    };
+    bool w_castle[4] = {true};
+    bool b_castle[4] = {false};
+    int en_passant = -1;
+    std::vector<int> trajectory;
+
+}
