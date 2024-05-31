0f6a2487f6691931b2e30b3a6069000b22f6ceef
Loszi <jiozzi11@gmail.com>
2024-05-31 18:28:15 -0400

    finished unit tests

--- tests/movement_test.cpp
+++ tests/movement_test.cpp
@@ -13,6 +13,12 @@ void SAME_BOARD(int board1[8][8], int board2[8][8]) {
     }
 }
 
+void SAME_CASTLE(bool castle1[4], bool castle2[4]) {
+    for (int i=0; i < 4; i++) {
+        ASSERT_EQ(castle1[i], castle2[i]);
+    }
+}
+
 //tests
 TEST(Movement, MovePiece) {
     int board[8][8] = {
@@ -860,13 +866,555 @@ TEST(Movement, Legal) {
     //needs to be most extensive test
     //some cases to consider:
     /*
-    1. pieces pinned to king
-    2. king moving into check
-    3. moving pieces while in check that do not block
-    4. moving pieces while in check that do block
-    5. en passant to reveal king by eating pawn blocking check via enemy piece
-    6. make sure it doesnt stop any moves that are actually legal (taking piece to block check) (or just moving normally)
+    1. pieces pinned to king | DONE
+    2. king moving into check | DONE
+    3. moving pieces while in check that do not block | DONE
+    4. moving pieces while in check that do block | DONE
+    5. en passant to reveal king by eating pawn blocking check via enemy piece | DONE
+    6. make sure it doesnt stop any moves that are actually legal (taking piece to block check) | DONE
     */
+    int board[8][8] = {
+        {0, 0, 0, 0, -6, 0, 0, 0},
+        {0, 0, 0, -1, -1, -1, 0, 0},
+        {0, 0, 2, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, -2, 0},
+        {0, 0, 0, 1, 1, 1, 0, 0},
+        {0, 0, 0, 0, 6, 0, 0, 0},
+    };
+    //pawn is pinned by bishop
+    bool w_turn = true;
+    int pos = 65;
+    bool castle[4] = {false}; //legality of castling checked in other function to never leave in check
+    int en_passant = -1;
+    std::vector<int> legal;
+    std::vector<int> correct;
+    legal = get_legal_moves(board, pos, castle, castle, w_turn, en_passant);
+    correct.push_back(56);
+    ASSERT_EQ(legal, correct);
+    correct.clear();
+    w_turn = false;
+    pos = 13;
+    legal = get_legal_moves(board, pos, castle, castle, w_turn, en_passant);
+    correct.push_back(22);
+    ASSERT_EQ(legal, correct);
+    correct.clear();
 
-    //get_legal_moves(int board[8][8], int pos, bool w_castle[4], bool b_castle[4], bool w_turn, int en_passant)
+    int board2[8][8] = {
+        {0, 0, 0, -1, -6, 0, 0, 0},
+        {0, 0, 0, -4, -1, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, -2, 0},
+        {0, 0, 0, 0, 1, 4, 0, 0},
+        {0, 0, 0, 0, 6, 1, 0, 0}, //both kings cant move into rook check
+    };
+    w_turn = true;
+    pos = 74;
+    legal = get_legal_moves(board2, pos, castle, castle, w_turn, en_passant);
+    ASSERT_TRUE(legal.empty());
+    w_turn = false;
+    pos = 4;
+    legal = get_legal_moves(board2, pos, castle, castle, w_turn, en_passant);
+    ASSERT_TRUE(legal.empty());
+
+    int board3[8][8] = {
+        {0, 0, 0, 0, -6, 0, 0, 4}, //both in check, shouldnt be able to move another piece
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {-1, 0, 0, 0, 0, 0, 0, 0}, //pawns cant move
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {1, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 6, 0, 0, -4}, 
+    };
+    w_turn = true;
+    pos = 50;
+    legal = get_legal_moves(board3, pos, castle, castle, w_turn, en_passant);
+    ASSERT_TRUE(legal.empty());
+    w_turn = false;
+    pos = 20;
+    legal = get_legal_moves(board3, pos, castle, castle, w_turn, en_passant);
+    ASSERT_TRUE(legal.empty());
+
+    int board4[8][8] = {
+        {0, 0, 0, 0, -6, 0, 0, 4}, //both in check, can block with rook
+        {0, 0, 0, 0, 0, 0, -4, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 4, 0},
+        {0, 0, 0, 0, 6, 0, 0, -4}, 
+    };
+    w_turn = true;
+    pos = 66;
+    correct.push_back(76);
+    legal = get_legal_moves(board4, pos, castle, castle, w_turn, en_passant);
+    ASSERT_EQ(correct, legal);
+    correct.clear();
+
+    w_turn = false;
+    pos = 16;
+    legal = get_legal_moves(board4, pos, castle, castle, w_turn, en_passant);
+    correct.push_back(6);
+    ASSERT_EQ(correct, legal);
+    correct.clear();
+
+    int board5[8][8] = {
+        {0, 0, 0, 0, 0, 0, 0, 0}, //both in check, can block with rook
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0}, //taking en passant reveals king
+        {6, 1, -1, 0, 0, 0, 0, -4},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {4, 0, 0, 0, 1, -1, 0, -6}, //same for black
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0}, 
+    };
+    en_passant = 22;
+    w_turn = true;
+    pos = 31;
+    correct.push_back(21);
+    legal = get_legal_moves(board5, pos, castle, castle, w_turn, en_passant);
+    ASSERT_EQ(correct, legal);
+    correct.clear();
+
+    w_turn = false;
+    en_passant = 65;
+    pos = 55;
+    correct.push_back(65);
+    legal = get_legal_moves(board5, pos, castle, castle, w_turn, en_passant);
+    ASSERT_EQ(correct, legal);
+    correct.clear();
+}
+
+TEST(Movement, AllLegal) {
+    //just test that first index is og piece and of correct color
+    int board[8][8] = {
+        {-4, -3, -2, -5, -6, -2, -3, -4},
+        {-1, -1, -1, -1, -1, -1, -1, -1},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {1, 1, 1, 1, 1, 1, 1, 1},
+        {4, 3, 2, 5, 6, 2, 3, 4},
+    };
+    bool w_turn = true;
+    bool castle[4] = {false}; //legality of castling checked in other function to never leave in check
+    int en_passant = -1;
+    std::vector<std::vector<int>> all_legal;
+    int pos;
+    all_legal = get_all_legal_moves(board, castle, castle, w_turn, en_passant);
+    for (int i=0; i < all_legal.size(); i++) {
+        pos = all_legal[i][0];
+        ASSERT_TRUE(board[pos/10][pos%10] > 0);
+    }
+
+    w_turn = false;
+    all_legal = get_all_legal_moves(board, castle, castle, w_turn, en_passant);
+    for (int i=0; i < all_legal.size(); i++) {
+        pos = all_legal[i][0];
+        ASSERT_TRUE(board[pos/10][pos%10] < 0);
+    }
+}
+
+TEST(Movement, KingCoord) {
+    int board[8][8] = {
+        {-4, -3, -2, -5, -6, -2, -3, -4},
+        {-1, -1, -1, -1, -1, -1, -1, -1},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {1, 1, 1, 1, 1, 1, 1, 1},
+        {4, 3, 2, 5, 6, 2, 3, 4},
+    };
+    std::vector<int> coords = get_king_coord(board);
+    ASSERT_EQ(coords[0], 74);
+    ASSERT_EQ(coords[1], 4);
+
+    int board2[8][8] = {
+        {-4, -3, -2, -5, 0, -2, -3, -4},
+        {-1, -1, -1, -1, -1, -1, -1, -1},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 6, 0, 0, 0, 0, -6, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {1, 1, 1, 1, 1, 1, 1, 1},
+        {4, 3, 2, 5, 0, 2, 3, 4},
+    };
+    coords = get_king_coord(board2);
+    ASSERT_EQ(coords[0], 41);
+    ASSERT_EQ(coords[1], 46);
+}
+
+TEST(Movement, InCheck) {
+    int board[8][8] = {
+        {-4, -3, -2, -5, -6, -2, -3, -4},
+        {-1, -1, -1, -1, -1, -1, -1, -1},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {1, 1, 1, 1, 1, 1, 1, 1},
+        {4, 3, 2, 5, 6, 2, 3, 4},
+    };
+    bool w_turn = true;
+    ASSERT_FALSE(in_check(board, w_turn));
+    w_turn = false;
+    ASSERT_FALSE(in_check(board, w_turn));
+
+    int board2[8][8] = {
+        {0, -3, -2, -5, -6, -2, -3, -4},
+        {-1, -1, -1, -1, 0, -1, -1, -1},
+        {0, 0, 0, 0, 4, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, -4, 0, 0, 0},
+        {1, 1, 1, 1, 0, 1, 1, 1},
+        {4, 3, 2, 5, 6, 2, 3, 0},
+    };
+    w_turn = true;
+    ASSERT_TRUE(in_check(board2, w_turn));
+    w_turn = false;
+    ASSERT_TRUE(in_check(board2, w_turn));
+}
+
+TEST(Movement, CastleConditions) {
+    //to test:
+    /*1. both permanents becoming false (king move rook move) | DONE
+    2. temps going false when one of squares under attack or piece in the way
+    */
+    //check_castle_conditions(board, castle, w_turn)
+    int board[8][8] = {
+        {-4, -3, -2, -5, -6, -2, -3, -4},
+        {-1, -1, -1, -1, -1, -1, -1, -1},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {1, 1, 1, 1, 1, 1, 1, 1},
+        {4, 3, 2, 5, 6, 2, 3, 4},
+    };
+    bool w_turn = true;
+    bool w_castle[4] = {false, true, false, true}; //[right temp, right perm, left temp, left perm]
+    bool b_castle[4] = {false, true, false, true};
+    check_castle_conditions(board, w_castle, w_turn);
+    bool correct[4] = {false, true, false, true};
+    SAME_CASTLE(w_castle, correct);
+    w_turn = false;
+    check_castle_conditions(board, b_castle, w_turn);
+    SAME_CASTLE(b_castle, correct);
+
+    int board1[8][8] = { //right rooks moved
+        {-4, -3, -2, -5, -6, -2, -3, 0},
+        {-1, -1, -1, -1, -1, -1, -1, 0},
+        {0, 0, 0, 0, 0, 0, 0, -4},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 4},
+        {1, 1, 1, 1, 1, 1, 1, 0},
+        {4, 3, 2, 5, 6, 2, 3, 0},
+    };
+    w_turn = true;
+    bool correct1[4] = {false, false, false, true};
+    check_castle_conditions(board1, w_castle, w_turn);
+    SAME_CASTLE(w_castle, correct1);
+    w_turn = false;
+    check_castle_conditions(board1, b_castle, w_turn);
+    SAME_CASTLE(b_castle, correct1);
+
+    int board2[8][8] = { //left rooks moved
+        {0, -3, -2, -5, -6, -2, -3, -4},
+        {0, -1, -1, -1, -1, -1, -1, -1},
+        {-4, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {4, 0, 0, 0, 0, 0, 0, 0},
+        {0, 1, 1, 1, 1, 1, 1, 1},
+        {0, 3, 2, 5, 6, 2, 3, 4},
+    };
+    w_turn = true;
+    bool w_castle2[4] = {false, true, false, true};
+    bool b_castle2[4] = {false, true, false, true};
+    bool correct2[4] = {false, true, false, false}; //left perm false
+    check_castle_conditions(board2, w_castle2, w_turn);
+    SAME_CASTLE(w_castle2, correct2);
+    w_turn = false;
+    check_castle_conditions(board2, b_castle2, w_turn);
+    SAME_CASTLE(b_castle2, correct2);
+
+    int board3[8][8] = { //kings moved
+        {-4, -3, -2, -5, 0, -2, -3, -4},
+        {-1, -1, -1, -1, -6, -1, -1, -1},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {1, 1, 1, 1, 6, 1, 1, 1},
+        {4, 3, 2, 5, 0, 2, 3, 4},
+    };
+    w_turn = true;
+    bool w_castle3[4] = {false, true, false, true};
+    bool b_castle3[4] = {false, true, false, true};
+    bool correct3[4] = {false}; //left perm false
+    check_castle_conditions(board3, w_castle3, w_turn);
+    SAME_CASTLE(w_castle3, correct3);
+    w_turn = false;
+    check_castle_conditions(board3, b_castle3, w_turn);
+    SAME_CASTLE(b_castle3, correct3);
+
+    //temps
+    int board4[8][8] = { //nothing in way
+        {-4, 0, 0, 0, -6, 0, 0, -4},
+        {-1, -1, -1, -1, -1, -1, -1, -1},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {1, 1, 1, 1, 1, 1, 1, 1},
+        {4, 0, 0, 0, 6, 0, 0, 4},
+    };
+    w_turn = true;
+    bool w_castle4[4] = {false, true, false, true};
+    bool b_castle4[4] = {false, true, false, true};
+    bool correct4[4] = {true, true, true, true};
+    check_castle_conditions(board4, w_castle4, w_turn);
+    SAME_CASTLE(w_castle4, correct4);
+    w_turn = false;
+    check_castle_conditions(board4, b_castle4, w_turn);
+    SAME_CASTLE(b_castle4, correct4);
+
+    int board5[8][8] = { //right path blocked
+        {-4, 0, 0, 0, -6, 0, 0, -4},
+        {-1, -1, -1, -1, -1, 0, -1, -1},
+        {0, 0, 0, 0, 0, 5, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, -5, 0, 0},
+        {1, 1, 1, 1, 1, 0, 1, 1},
+        {4, 0, 0, 0, 6, 0, 0, 4},
+    };
+    w_turn = true;
+    bool w_castle5[4] = {false, true, false, true};
+    bool b_castle5[4] = {false, true, false, true};
+    bool correct5[4] = {false, true, true, true};
+    check_castle_conditions(board5, w_castle5, w_turn);
+    SAME_CASTLE(w_castle5, correct5);
+    w_turn = false;
+    check_castle_conditions(board5, b_castle5, w_turn);
+    SAME_CASTLE(b_castle5, correct5);
+
+    int board6[8][8] = { //left path blocked
+        {-4, 0, 0, 0, -6, 0, 0, -4},
+        {-1, -1, -1, 0, -1, -1, -1, -1},
+        {0, 0, 0, 5, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, -5, 0, 0, 0, 0},
+        {1, 1, 1, 0, 1, 1, 1, 1},
+        {4, 0, 0, 0, 6, 0, 0, 4},
+    };
+    w_turn = true;
+    bool w_castle6[4] = {false, true, false, true};
+    bool b_castle6[4] = {false, true, false, true};
+    bool correct6[4] = {true, true, false, true};
+    check_castle_conditions(board6, w_castle6, w_turn);
+    SAME_CASTLE(w_castle6, correct6);
+    w_turn = false;
+    check_castle_conditions(board6, b_castle6, w_turn);
+    SAME_CASTLE(b_castle6, correct6);
+
+    int board7[8][8] = { //both in check
+        {-4, 0, 0, 0, -6, 0, 0, -4},
+        {-1, -1, -1, -1, 0, -1, -1, -1},
+        {0, 0, 0, 0, 5, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, -5, 0, 0, 0},
+        {1, 1, 1, 1, 0, 1, 1, 1},
+        {4, 0, 0, 0, 6, 0, 0, 4},
+    };
+    w_turn = true;
+    bool w_castle7[4] = {false, true, false, true};
+    bool b_castle7[4] = {false, true, false, true};
+    bool correct7[4] = {false, true, false, true};
+    check_castle_conditions(board7, w_castle7, w_turn);
+    SAME_CASTLE(w_castle7, correct7);
+    w_turn = false;
+    check_castle_conditions(board7, b_castle7, w_turn);
+    SAME_CASTLE(b_castle7, correct7);
+}
+
+TEST(Movement, GameState) {
+    //check with edge cases like en passant last move to prevent state mate after pawn checks
+    //check regular checkmate and stalemate of both sides
+    //0 = regular , 1 = checkmate, 2 = stalemate
+
+    //regular board
+    int board[8][8] = {
+        {-4, -3, -2, -5, -6, -2, -3, -4},
+        {-1, -1, -1, -1, -1, -1, -1, -1},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {1, 1, 1, 1, 1, 1, 1, 1},
+        {4, 3, 2, 5, 6, 2, 3, 4},
+    };
+    int game_over = 0;
+    bool w_check = false;
+    bool b_check = false;
+    bool w_turn = true;
+    int en_passant = -1;
+    check_game_state(board, game_over, w_check, b_check, w_turn, en_passant); //game over by ref
+    ASSERT_EQ(game_over, 0);
+    w_turn = false;
+    check_game_state(board, game_over, w_check, b_check, w_turn, en_passant);
+    ASSERT_EQ(game_over, 0);
+
+    int board2[8][8] = { //black gets checkmate
+        {0, 0, 0, 0, -6, 0, 0, 0},
+        {0, 0, 0, 0, 5, 0, 0, 0},
+        {0, 0, 0, 0, 6, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+    };
+    w_turn = false;
+    b_check = true;
+
+    check_game_state(board2, game_over, w_check, b_check, w_turn, en_passant); 
+    ASSERT_EQ(game_over, 1);
+    game_over = 0;
+    b_check = false;
+
+    int board3[8][8] = { //white gets checkmate
+        {0, 0, 0, 0, 6, 0, 0, 0},
+        {0, 0, 0, 0, -5, 0, 0, 0},
+        {0, 0, 0, 0, -6, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+    };
+    w_turn = true;
+    w_check = true;
+
+    check_game_state(board3, game_over, w_check, b_check, w_turn, en_passant); 
+    ASSERT_EQ(game_over, 1);
+    game_over = 0;
+    w_check = false;
+
+    int board4[8][8] = { //black gets stalemate
+        {0, 0, 0, 0, -6, 0, 0, 0},
+        {0, 0, 1, 0, 0, 0, 1, 0},
+        {0, 0, 0, 0, 6, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+    };
+    w_turn = false;
+
+    check_game_state(board4, game_over, w_check, b_check, w_turn, en_passant); 
+    ASSERT_EQ(game_over, 2);
+    game_over = 0;
+
+    int board5[8][8] = { //white gets stalemate
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, -6, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, -6, 0, 0, 0},
+        {0, 0, -1, 0, 0, 0, -1, 0},
+        {0, 0, 0, 0, 6, 0, 0, 0},
+    };
+    w_turn = true;
+
+    check_game_state(board5, game_over, w_check, b_check, w_turn, en_passant); 
+    ASSERT_EQ(game_over, 2);
+    game_over = 0;
+
+    //en passant edge case:
+    int board6[8][8] = { 
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, -1, -1, -1, 0, 0, 0},
+        {0, 0, -1, -6, -1, 0, 0, 0},
+        {0, -1, 1, -1, -1, 0, 0, 0},
+        {0, 0, 0, 4, 1, 1, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 2},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+    }; //en passant only thing stopping stalemate
+    en_passant = 52;
+    w_turn = false;
+    check_game_state(board6, game_over, w_check, b_check, w_turn, en_passant); 
+    ASSERT_EQ(game_over, 0);
+}
+
+TEST(Movement, EnPassant) {
+    int board[8][8] = {
+        {-4, -3, -2, -5, -6, -2, -3, -4},
+        {-1, -1, -1, -1, 0, -1, -1, -1},
+        {0, 0, 0, 0, -1, 0, 0, 0}, //no en passant only one
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 1, 0, 0, 0},
+        {1, 1, 1, 1, 0, 1, 1, 1},
+        {4, 3, 2, 5, 6, 2, 3, 4},
+    };
+    int start_pos;
+    int end_pos;
+    start_pos = 64;
+    end_pos = 64;
+    ASSERT_EQ(check_en_passant(board, start_pos, end_pos), -1);
+    start_pos = 14;
+    end_pos = 24;
+    ASSERT_EQ(check_en_passant(board, start_pos, end_pos), -1);
+
+    int board2[8][8] = {
+        {-4, -3, -2, -5, -6, -2, -3, -4},
+        {-1, -1, -1, -1, 0, -1, -1, -1},
+        {0, 0, 0, 0, 0, 0, 0, 0}, //both en passant
+        {0, 0, 0, 0, -1, 0, 0, 0},
+        {0, 0, 0, 0, 1, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {1, 1, 1, 1, 0, 1, 1, 1},
+        {4, 3, 2, 5, 6, 2, 3, 4},
+    };
+    start_pos = 64;
+    end_pos = 44;
+    ASSERT_EQ(check_en_passant(board2, start_pos, end_pos), 54);
+
+    start_pos = 14;
+    end_pos = 34;
+    ASSERT_EQ(check_en_passant(board2, start_pos, end_pos), 24);
+
+    //check with rook instead of pawn 
+    int board3[8][8] = {
+        {-4, -3, -2, -5, -6, -2, -3, -4},
+        {-1, -1, -1, -1, 0, -1, -1, -1},
+        {0, 0, 0, 0, 0, 0, 0, 0}, //wrong piece
+        {0, 0, 0, 0, -4, 0, 0, 0},
+        {0, 0, 0, 0, 4, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {1, 1, 1, 1, 0, 1, 1, 1},
+        {4, 3, 2, 5, 6, 2, 3, 4},
+    };
+    start_pos = 64;
+    end_pos = 44;
+    ASSERT_EQ(check_en_passant(board3, start_pos, end_pos), -1);
+
+    start_pos = 14;
+    end_pos = 34;
+    ASSERT_EQ(check_en_passant(board3, start_pos, end_pos), -1);
 }
